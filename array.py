0e6a9e127ba4a6bb3662fc5c94dc151c6c69733d
Rama Govindu Pamisetty-R161194 <58973659+Rama-Govindu-Pamisetty-R161194@users.noreply.github.com>
2019-12-18 11:55:12 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,11 @@
+import numpy as np
+a=[10,9,8,7,6]
+b=[5,4,3,2,1]
+c=len(a)
+for i in range(0,len(b)):
+	 a.append(b[i])
+print a
+c=np.array([10,9,8,7,6])
+d=np.array([5,4,3,2,1])
+e=np.append(c,d)
+print e
